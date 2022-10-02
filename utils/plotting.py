ea3a341ca2cdbd7f1d3c6bc0448d26fa03e6699e
Acusick1 <andrewcusick13@outlook.com>
2022-10-02 16:40:19 +0100

    Bug fix: Division by zero in get_subplot_shape.

--- utils/plotting.py
+++ utils/plotting.py
@@ -3,7 +3,11 @@ import numpy as np
 
 def get_subplot_shape(num: int, max_columns: int = 8) -> np.array:
 
-    columns = min(round(np.sqrt(num)), max_columns)
-    quotient, rem = divmod(num, columns)
-    rows = quotient + 1 if rem else quotient
+    if num:
+        columns = min(round(np.sqrt(num)), max_columns)
+        quotient, rem = divmod(num, columns)
+        rows = quotient + 1 if rem else quotient
+    else:
+        rows, columns = 1, 1
+
     return np.array((rows, columns))
