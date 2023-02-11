78d18cf53ed581002e2d61401ac4e7a3e32061f2
Andrew Cusick <andrewcusick13@outlook.com>
2023-02-11 16:30:54 +0000

    feat: added example MACD crossover strategy

--- strategies/daily.py
+++ strategies/daily.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from backtesting import Backtest, Strategy
-from backtesting.lib import crossover
+from backtesting.lib import crossover, TrailingStrategy
 from backtesting.test import GOOG
 
 
@@ -31,12 +31,49 @@ class SmaCross(Strategy):
             self.sell()
 
 
+class MacdSignalCross(TrailingStrategy):
+
+    macd = None
+    singal = None
+
+    def init(self):
+        # Precompute macd and signal
+        self.macd, self.signal = self.I(macd, self.data.Close)
+        self.set_trailing_sl(0.0001)
+
+    def next(self):
+        # If sma1 crosses above sma2, close any existing
+        # short trades, and buy the asset
+        if crossover(self.macd, self.signal):
+            self.position.close()
+            self.buy()
+
+        # Else, if sma1 crosses below sma2, close any existing
+        # long trades, and sell the asset
+        elif crossover(self.signal, self.macd):
+            self.position.close()
+            self.sell()
+
+
 def simple_moving_avg(values, n):
     """
     Return simple moving average of values, at
     each step taking into account n previous values.
     """
     return pd.Series(values).rolling(n).mean()
+
+
+def macd(values, n_fast: int = 12, n_slow: int = 26, smooth: int = 9):
+    """
+    Return MACD (Moving Average Convergence/Divergence)
+    using fast and slow exponential moving averages.
+    """
+    emaslow = pd.Series(values).ewm(span=n_slow, adjust=False).mean()
+    emafast = pd.Series(values).ewm(span=n_fast, adjust=False).mean()
+    macd = emafast - emaslow
+    signal = macd.ewm(span=smooth, adjust=False).mean()
+
+    return macd, signal
 
 
 if __name__ == "__main__":
