90b825c4a38a63582976fe78695f95b725117017
Andrew Cusick <andrewcusick13@outlook.com>
2023-03-07 20:07:42 +0000

    feat: get S&P500 tickers

---
+++ utils/tickers.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def get_snp500():
+    df = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+    return df["Symbol"].to_list()
+
+
+if __name__ == "__main__":
+    tickers = get_snp500()
+    print(tickers)
